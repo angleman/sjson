f86bfbb037b31faf3ae0f7bcccb810a7cb128719
angleman <angleman@cormea.com>
2013-09-11 21:47:43 -0600

    add tests

--- test/_loads.js
+++ test/_loads.js
@@ -2,7 +2,25 @@
 var should = require('should');
 
 describe('package', function() {
-    describe('loads', function() {
-        var app = require('../index.js');
+    describe('should', function() {
+    	var data = {first:"joe", last:"smith", age:27};
+    	var json  = JSON.stringify(data);
+    	var squished = '{first:"joe",last:"smith",age:27}';
+    	var SJSON, sjson, json2;
+    	it('load', function() {
+	        SJSON = require('../index.js');
+        });
+
+    	it('squish to: ' + squished, function() {
+			sjson = SJSON.squish(json);
+			should.exist(sjson);
+			sjson.should.equal(squished);
+        });
+
+    	it('parse back to: ' + json, function() {
+			json2 = SJSON.parse(sjson);
+			should.exist(json2);
+			json2.should.equal(json);
+        });
     });
 });
