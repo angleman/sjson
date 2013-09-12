7430f2c854b1bffbbc85a32f657725b118d00a37
angleman <angleman@cormea.com>
2013-09-11 21:47:55 -0600

    debug parse

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 // adapted from: http://stackoverflow.com/questions/11233498/json-stringify-without-quotes-on-properties
-function sjsonSmallify(data) {
-    var json = JSON.stringify(data);
-    json = json.replace(/\"([^(\")"]+)\":/g,"$1:");
+function sjsonSmallify(json) {
+    var sjson = json.replace(/\"([^(\")"]+)\":/g,"$1:");
+    return sjson;
+}
+
+
+
+function sjsonParse(sjson) {
+    var json = sjson.replace(/(\{|,)\s*(.+?)\s*:/g, '$1"$2":');
     return json;
 }
 
 
 
-function sjsonParse(str) {
-    var val = str.replace(/(\{|,)\s*(.+?)\s*:/g, '$1 "$2":');
-    var data = JSON.parse(val);
-    return data;
-}
+module.exports.squish = sjsonSmallify;
 
-
-
-module.exports.smallify = sjsonSmallify;
-
-module.exports.parse    = sjsonParse;
+module.exports.parse  = sjsonParse;
